7c9333528156588d4bd9aea2c9c3c2678704777b
casino777777 <137805551+casino777777@users.noreply.github.com>
2024-02-02 16:57:53 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      'sm': '480px',
+      'md': '768px',
+      'lg': '976px',
+      'xl': '1440px',
+      '2xl': '1920px',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
